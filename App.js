c9e619af704c924a8f04660365d7449659fa2305
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-06-29 21:27:08 +0200

    ok

--- App.js
+++ App.js
@@ -1,10 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Platform } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Привет, мир!</Text>
+    
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +18,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 10,
+    color: 'red',
   },
 });
