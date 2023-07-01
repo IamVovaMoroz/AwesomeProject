1bc79616e0e51a4ff7f8680a19e3ec3856843a67
IamVovaMoroz <113520901+IamVovaMoroz@users.noreply.github.com>
2023-07-01 19:52:31 +0200

    ok

--- App.js
+++ App.js
@@ -1,20 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Platform, Button, ImageBackground, Image } from 'react-native';
+
+// import { styles } from "./styles";
 import BigButton from './BigButton';
 import { defaultStyles } from './assets/fonts/fonts';
-
-// const availableFonts = Text.defaultProps?.style?.fontFamily;
-// console.log(availableFonts);
+import { useFonts } from "expo-font";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    "Roboto-Regular": require("./assets/fonts/Roboto/Roboto-Regular.ttf"),
+    "Roboto-Medium": require("./assets/fonts/Roboto/Roboto-Medium.ttf"),
+    "Roboto-Bold": require("./assets/fonts/Roboto/Roboto-Bold.ttf"),
+    // 1: require("./assets/fonts/Roboto/1.ttf"),
+  });
+
   return (
+    
     <ImageBackground
       source={{ uri: 'https://i.pinimg.com/236x/c8/d7/c1/c8d7c18a6a9fd2c382299b12ef7c86c7.jpg' }}
       style={styles.backgroundImage}
     >
       <View style={styles.container}>
         <Text style={defaultStyles.headingText}>Open new world!</Text>
-        <Text style={defaultStyles.normalText}>Привет, мир!!</Text>
+        <Text style={styles.text}>Привет, мир!!</Text>
         <View style={styles.imageContainer}>
           <Image source={require('./minecraft.png')} style={styles.image} />
         </View>
@@ -41,7 +49,20 @@ const styles = StyleSheet.create({
     fontSize: 20,
     fontWeight: 'bold',
     marginTop: 10,
+    color: 'blue',
+  },
+  headingText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    fontFamily: 'Roboto-Bold',
+    marginTop: 10,
     color: 'green',
+  },
+  normalText: {
+    fontSize: 16,
+    fontFamily: 'Roboto-Medium',
+    marginTop: 5,
+    color: 'white',
   },
   imageContainer: {
     position: 'absolute',
